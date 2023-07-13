f9be229d154781ea62ab2e8b1768c46e654e513e
victor-ok <vua.okafor@gmail.com>
2023-07-13 16:00:08 +0100

    worked on the remaining tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code
+ *
+ * Return: Always 0 if Successful
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
