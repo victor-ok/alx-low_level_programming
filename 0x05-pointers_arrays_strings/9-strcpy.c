08984daf7bb37948457654d761f05f8b3f0c7192
victor-ok <vua.okafor@gmail.com>
2023-07-24 19:58:41 +0100

    worked on random password

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,18 +13,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		
-	    if(src[i]=='\0') {
-	        src[i++]='\\';
-	        src[i++]='0';
-	        i++;
-	    }
-	    else {
-	        dest[i] = src[i];
-		i++;
-	    }
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
