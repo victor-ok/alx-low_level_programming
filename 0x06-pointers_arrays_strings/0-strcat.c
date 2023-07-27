429d036a7662910cd602d124d7b046fbe30aafde
victor-ok <vua.okafor@gmail.com>
2023-07-27 10:16:08 +0100

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * _strcat - function to append strings
+ * @dest: the string to append.
+ * @src: the main string.
+ * Return: dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *n = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+
+	*dest = '\0';
+
+	return (n);
+}
