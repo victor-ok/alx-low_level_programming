25da4ab39cb4f02096896c165d9eed3888273187
victor-ok <vua.okafor@gmail.com>
2023-08-10 21:37:02 +0100

    fixed comment on 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,6 +6,10 @@
  * @ptr: pointer to the memory previously allocated.
  * @old_size: size in bytes of the allocated space for ptr.
  * @new_size: size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
