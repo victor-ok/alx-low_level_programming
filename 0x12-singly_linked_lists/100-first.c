a49bccd5bcf335e58fee4dc6f47c68a84ddd1000
victor-ok <vua.okafor@gmail.com>
2023-08-25 10:13:08 +0100

    fixed constructor function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * construct - prints before executing main
  */
 
-void construct(void)
+void printBeforeMain (void) __attribute__((constructor));
+
+void printBeforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
